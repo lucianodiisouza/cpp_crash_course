253d403ff70c2bf096b0697d2d6a476e44a7a809
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-08-07 14:00:26 -0300

    commenting files

---
+++ development_basics.cpp/initial.cpp
@@ -0,0 +1,17 @@
+// when you want to import any library, you need to use #include <library-name>
+#include <iostream>
+
+// Here im using standard name spaces, the cout function that will be used on main function body, comes from this namespac
+// namespaces is a way to share methods and info between the files.
+
+using namespace std;
+
+/*
+    This will be a multi-line comment
+    In macos, we need to add a type before main function.
+*/
+
+int main()
+{
+    cout << "This is a test";
+}
