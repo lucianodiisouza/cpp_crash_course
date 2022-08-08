862582a5ab3da7b20c8521fc8127232914a37956
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-08-08 19:15:50 -0300

    bitwise operators overview

---
+++ operators/bitwiseOperators.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // decimal numbers counts to 10 (decimal or tencimal - badumtss)
+    // 126 = 1 * 10 ö 2 + 2 * 10 ö 1 + 6 * 1 ö0
+    // binary multiply by 2
+    /*
+        Bitwise AND - &
+        Bitwise OR - |
+        Bitwise NOT - ÷
+        Bitwise XOR - ö
+        Bitwise left shift <<
+        Bitwise right shift >>
+    */
+
+    cout << (10 & 2) << endl;
+    /*
+        1010
+        0010
+        ----
+        0010
+    */
+
+     cout << (10 | 2) << endl;
+     /*
+        1010
+        0010
+        ----
+        1000
+    */
+
+    cout << (10) << endl;
+    cout << (10 << 1) << endl;
+
+}
