56fc2c7c23a8f5de23ab6c24e170831370d37850
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-08-07 22:09:05 -0300

    complete name program (input with cin)

---
+++ input_data/completeName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name, surname;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your surname: ";
+    cin >> surname;
+
+    cout << "Welcome, " << name << " " << surname << "." << endl;
+}
