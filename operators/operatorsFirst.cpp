5549b7278f0f8d5d2edf2bac816dc596c83ea994
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-08-08 17:53:50 -0300

    pre and post decrementations/incrementations

--- operators/operatorsFirst.cpp
+++ operators/operatorsFirst.cpp
@@ -3,12 +3,31 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    int b = 4;
+   /*
+        int a = 10;
+        int b = 4;
 
-    cout << "+ " << a + b << endl;
-    cout << "- " << a - b << endl;
-    cout << "* " << a * b << endl;
-    cout << "/ "<< a / b << endl;
-    cout << "% " << a % b << endl;
+        cout << "+ " << a + b << endl;
+        cout << "- " << a - b << endl;
+        cout << "* " << a * b << endl;
+        cout << "/ "<< a / b << endl;
+        cout << "% " << a % b << endl;
+    */
+
+    // incrementarion > increase by 1
+    // decrementarion > decrease by 1
+
+    int c = 1;
+    // c = c + 1;
+
+    c += 1;
+
+    cout <<  c << endl;
+
+
+    int d = 1;
+
+    cout << d++ << endl;
+    cout << ++d << endl;
+
 }
