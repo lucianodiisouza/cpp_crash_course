80c0e68e03d2fc9a512fe991cd029140d68b75d2
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-08-07 22:05:24 -0300

    how to input data

---
+++ input_data/input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    string name;
+
+    cout << "Hallo, type your name and press enter please: " << endl;
+    cin >> name;
+    cout << "now, type your age and press enter: " << endl;
+    cin >>  age;
+    cout << "Fine!" << endl;
+    cout << "Welcome " << name << ", you alive by " << age<< " years!" << endl;
+
+}
